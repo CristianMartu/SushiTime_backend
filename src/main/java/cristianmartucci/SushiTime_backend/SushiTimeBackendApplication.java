ea6a633a32f09352e0146f9a3d6e3bb8d35aa904
CristianMartu <martuccicristian18@gmail.com>
2024-07-15 10:48:24 +0200

    Initial commit

---
+++ src/main/java/cristianmartucci/SushiTime_backend/SushiTimeBackendApplication.java
@@ -0,0 +1,13 @@
+package cristianmartucci.SushiTime_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiTimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiTimeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cristianmartucci/SushiTime_backend/SushiTimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cristianmartucci.SushiTime_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiTimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
